376196271803a510ae70e709a54303ed65c3807d
Bryton Hall <email@bryton.io>
2023-03-29 22:22:15 -0400

    add workflow to build and publish website

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,15 +1,30 @@
+# Contributing
+
 Thanks for your interest in contributing!
+
 
 ## local dev
 
+Setup a local development environment (optionally/recommended, using only [nix](https://nixos.org/download.html#download-nix)).
+
 ```
-npm install
+nix develop
+npm i
 npm start
 ```
 
+
 ## package
 
+Build the webpages (into the `./result` directory) with
+
 ```
-npm install
-npm run build
+nix build
 ```
+
+
+## release
+
+Bump the version in [`package.json`](./package.json) and update [`CHANGELOG.md`](./CHANGELOG.md).
+
+A [GitHub Action](./.github/workflows/main.yml) will build and (if on the default branch) deploy the website.
--- flake.nix
+++ flake.nix
@@ -7,10 +7,32 @@
     inputs.utils.lib.eachDefaultSystem (system:
       let pkgs = inputs.nixpkgs.legacyPackages.${system}; in
       {
-        devShells.default = with pkgs; mkShell {
-          buildInputs = [
-            nodejs
+        packages.default = with pkgs; let
+          package = (with builtins; fromJSON (readFile ./package.json));
+        in
+        buildNpmPackage {
+          pname = package.name;
+          version = package.version;
+          src = ./.;
+          npmDepsHash = "sha256-AUJlV57cYu0jeMSk3y+3zO7/LCC+f0z5BQSrRKFsnp8=";
+          nativeBuildInputs = [
+            python310
+            nodejs.pkgs.node-gyp-build
+            pkg-config
           ];
+          buildInputs = [ ]
+            ++ (with xorg; [
+            libX11
+            libXi
+            libXext
+            libGL
+          ]);
+          installPhase = ''
+            cp -r www $out
+          '';
+        };
+        devShells.default = with pkgs; mkShell {
+          inputsFrom = builtins.attrValues self.packages.${system};
           shellHook = ''
             export PATH="$PATH:$PWD/node_modules/.bin"
           '';
