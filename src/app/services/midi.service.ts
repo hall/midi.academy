75635154977bbb1f542069ffda69e30ebc93e873
Bryton Hall <email@bryton.io>
2023-04-11 22:34:15 -0400

    use single input and output midi device

--- src/app/services/midi.service.ts
+++ src/app/services/midi.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { NotesService } from './notes.service';
 import { FeedbackService } from './feedback.service';
+import { CursorService } from './cursor.service';
 
 import MIDIAccess = WebMidi.MIDIAccess;
 import MIDIConnectionEvent = WebMidi.MIDIConnectionEvent;
@@ -18,114 +19,92 @@ export const MIDIOffset = 21;
 export class MidiService {
   public onChange: EventEmitter<any> = new EventEmitter<any>();
 
-  // MIDI Devices
-  available = true; // TODO: this is only to avoid the on-by-default error message
-  inputs: MIDIInput[] = [];
-  outputs: MIDIOutput[] = [];
   device = 'none';
-
-  // Initialize maps of notes coming from MIDI Input
-  mapNotesAutoPressed = new Map();
+  available = true; // TODO: this is only to avoid the on-by-default error message
+  private output?: MIDIOutput;
 
   // wakeLock used with Midi Input
   wakeLockObj?: WakeLockSentinel;
   wakeLockTimer?: number;
 
-  constructor(private notes: NotesService, private feedback: FeedbackService) {
-    // Initialize MIDI
-    navigator.requestMIDIAccess?.({ sysex: true }).then(this.success.bind(this), () => {
-      this.available = false;
-    });
-  }
+  private NOTE_ON = 9;
+  private NOTE_OFF = 8;
 
-  // Register MIDI Inputs' handlers and outputs
-  initDev(access: MIDIAccess): void {
-    access.inputs.forEach((input) => {
-      if (!input.name?.includes('Midi Through')) this.inputs.push(input);
-    });
+  constructor(private notes: NotesService, private feedback: FeedbackService, private cursor: CursorService) {
+    navigator.requestMIDIAccess?.({ sysex: true }).then((access) => {
+      this.available = true;
 
-    this.device = 'none';
-
-    this.inputs.forEach((port) => {
-      this.device = `${port.name} (${port.manufacturer})`;
-      port.onmidimessage = (event: MIDIMessageEvent) => {
-        const NOTE_ON = 9;
-        const NOTE_OFF = 8;
-        const cmd = event.data[0] >> 4;
-        // const channel = event.data[0] & 0xf;
-        let pitch = 0;
-        if (event.data.length > 1) pitch = event.data[1];
-        let velocity = 0;
-        if (event.data.length > 2) velocity = event.data[2];
-        if (cmd === NOTE_OFF || (cmd === NOTE_ON && velocity === 0)) {
-          this.noteOff(event.timeStamp, pitch);
-        } else if (cmd === NOTE_ON) {
-          this.noteOn(event.timeStamp, pitch);
-        }
+      access.onstatechange = (event: MIDIConnectionEvent) => {
+        this.onStateChange(event.target as MIDIAccess);
       };
-    });
 
-    access.outputs.forEach((output) => {
-      if (!output.name?.includes('Midi Through')) this.outputs.push(output);
+      this.onStateChange(access);
     });
   }
 
-  // Initialize MIDI event listeners
-  success(access: MIDIAccess): void {
-    this.available = true;
+  // if input is changed, create event listener on new input
+  set input(input: MIDIInput) {
+    if (input.name) this.device = input.name;
+    input.onmidimessage = (event: MIDIMessageEvent) => {
+      const cmd = event.data[0] >> 4;
+      // const channel = event.data[0] & 0xf;
 
-    access.onstatechange = (event: MIDIConnectionEvent) => {
-      this.initDev(event.target as MIDIAccess);
+      let pitch = 0;
+      if (event.data.length > 1) pitch = event.data[1];
+
+      let velocity = 0;
+      if (event.data.length > 2) velocity = event.data[2];
+
+      if (cmd === this.NOTE_OFF || (cmd === this.NOTE_ON && velocity === 0)) {
+        this.noteOff(event.timeStamp, pitch);
+      } else if (cmd === this.NOTE_ON) {
+        this.noteOn(event.timeStamp, pitch, velocity);
+      }
     };
-
-    this.initDev(access);
   }
 
-  pressNote(pitch: number, velocity: number): void {
-    this.mapNotesAutoPressed.set(pitch.toFixed(), 1);
-    const iter = this.outputs.values();
-    for (let output = iter.next(); !output.done; output = iter.next()) {
-      output.value.send([0x90, pitch, velocity], window.performance.now());
-    }
-    setTimeout(() => {
-      this.noteOn(Date.now() - this.notes.timePlayStart, pitch);
-    }, 0);
-    if (this.outputs.values().next().done) {
-      this.notes.piano.keyDown({ midi: pitch });
-    }
-  }
-
-  // Release note on Output MIDI Device
-  releaseNote(pitch: number): void {
-    this.mapNotesAutoPressed.delete(pitch.toFixed());
-    const iter = this.outputs.values();
-    for (let o = iter.next(); !o.done; o = iter.next()) {
-      o.value.send([0x80, pitch, 0x00], window.performance.now());
-    }
-    setTimeout(() => {
-      this.noteOff(Date.now() - this.notes.timePlayStart, pitch);
-    }, 0);
-    if (this.outputs.values().next().done) this.notes.piano.keyUp({ midi: pitch });
+  onStateChange(access: MIDIAccess): void {
+    // refresh input and output devices
+    this.input = Array.from(access.inputs.values()).filter((input) => !input.name?.includes('Midi Through'))[0];
+    this.output = Array.from(access.outputs.values()).filter((outputs) => !outputs.name?.includes('Midi Through'))[0];
   }
 
   // note pressed at time for pitch
-  noteOn(time: number, pitch: number /*, velocity: number*/): void {
-    this.refreshWakeLock();
+  noteOn(time: number, pitch: number, velocity: number): void {
     const name = pitch - MIDIOffset;
     this.notes.press(name);
 
     // wrong key pressed
-    if (!this.notes.keys[name].required) {
-      this.feedback.addText('❌', 0, 20);
+    if (this.cursor.running && !this.notes.keys[name].required) {
+      // this.feedback.addText('❌', 0, 20);
     }
 
     this.onChange.emit();
+    this.refreshWakeLock();
   }
 
-  // Midi input note released
+  // input note released
   noteOff(time: number, pitch: number): void {
     this.notes.release(pitch - MIDIOffset);
     this.onChange.emit();
+  }
+
+  pressNote(pitch: number, velocity: number): void {
+    this.output?.send([0x90, pitch, velocity]);
+
+    setTimeout(() => {
+      this.noteOn(Date.now() - this.notes.timePlayStart, pitch, velocity);
+    }, 0);
+    this.notes.piano.keyDown({ midi: pitch });
+  }
+
+  // Release note on Output MIDI Device
+  releaseNote(pitch: number): void {
+    this.output?.send([0x80, pitch, 0x00]);
+    setTimeout(() => {
+      this.noteOff(Date.now() - this.notes.timePlayStart, pitch);
+    }, 0);
+    this.notes.piano.keyUp({ midi: pitch });
   }
 
   // Refresh wake lock for two minutes
